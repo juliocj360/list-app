550d38a4d615c8abb6227c86e5613c77fb285a2c
juliocj360 <juliocj360@gmail.com>
2017-03-16 15:16:33 -0700

    User can see dashboard. #1

--- list-app/src/app/app.component.ts
+++ list-app/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { ListComponent } from './list';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>Julio's Lists</h1>
+    <app-list></app-list>
+  `,
   styles: [`
-    h1 {
-    color: blue;
-  }`]
+    h1,p {
+    font-family: 'Raleway', sans-serif;
+    }
+  `],
+  entryComponents: [ListComponent]
 })
 export class AppComponent {
-  title = 'Welcome to Julio\'s shopping list!';
 }
